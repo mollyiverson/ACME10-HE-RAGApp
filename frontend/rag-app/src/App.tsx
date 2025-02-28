e3ccb2d99caac804c57e632a7a93b301e9a433e7
Chandler Juego <chandlerjuego101@gmail.com>
2025-02-28 13:05:14 -0800

    Dbpedia data validation check after doing dbpedia query run

--- frontend/rag-app/src/App.tsx
+++ frontend/rag-app/src/App.tsx
@@ -164,8 +164,13 @@ function App() {
       let dbpediaMessage: Message = {text: "", sender: "bot"}
       if (nlpData.sparql_query) {
         const dbpediaData = await callDbpediaFunction(nlpData.sparql_query);
+        
+        // Ensure dbpeida data has valid structure and text
+        const firstBinding = dbpediaData?.results?.bindings?.[0];
+        const abstractText = firstBinding?.abstract?.value || "No abstract available.";      
+
         dbpediaMessage = {
-          text: `DBpedia Abstract: ${dbpediaData.results.bindings[0].abstract.value}`,
+          text: `DBpedia Abstract: ${abstractText}`,
           sender: 'bot',
         };
         setMessages((prevMessages) => [...prevMessages, dbpediaMessage]);
